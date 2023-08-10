d517dab1895d42fa89b6bb1dc5abc5fa54c0885d
Michael2718 <isaevmihail007@gmail.com>
2023-08-10 15:26:47 +0300

    Update MarsPhotosApp and implement share photo feature

--- app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
+++ app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
@@ -18,54 +18,201 @@
 
 package com.example.marsphotos.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.FileProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import coil.ImageLoader
+import coil.request.ImageRequest
 import com.example.marsphotos.R
+import com.example.marsphotos.network.MarsPhoto
 import com.example.marsphotos.ui.screens.HomeScreen
 import com.example.marsphotos.ui.screens.MarsViewModel
+import com.example.marsphotos.ui.screens.PhotoScreen
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+
+enum class MarsPhotosScreen {
+    Start,
+    Photo
+}
 
 @Composable
-fun MarsPhotosApp() {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+fun MarsPhotosApp(
+    modifier: Modifier = Modifier,
+    viewModel: MarsViewModel = viewModel(factory = MarsViewModel.Factory),
+    navController: NavHostController = rememberNavController()
+) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = MarsPhotosScreen.valueOf(
+        backStackEntry?.destination?.route ?: MarsPhotosScreen.Start.name
+    )
+    val uiState by viewModel.uiState.collectAsState()
+    val intentContext = LocalContext.current
+
     Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = { MarsTopAppBar(scrollBehavior = scrollBehavior) }
-    ) {
-        Surface(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it)
+        topBar = {
+            MarsTopAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() },
+                onShareButtonClicked = {
+                    viewModel.viewModelScope.launch {
+                        sharePhoto(
+                            intentContext = intentContext,
+                            photo = uiState.currentPhoto!!
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = MarsPhotosScreen.Start.name,
+            modifier = modifier.padding(innerPadding)
         ) {
-            val marsViewModel: MarsViewModel = viewModel()
-            HomeScreen(
-                marsUiState = marsViewModel.marsUiState)
+            composable(MarsPhotosScreen.Start.name) {
+                HomeScreen(
+                    marsUiState = uiState.requestStatus,
+                    retryAction = viewModel::getMarsPhotos,
+                    onItemClick = {
+                        navController.navigate(MarsPhotosScreen.Photo.name)
+                        viewModel.updateCurrentPhoto(it)
+                    }
+                )
+            }
+            composable(MarsPhotosScreen.Photo.name) {
+                PhotoScreen(photo = uiState.currentPhoto!!)
+            }
         }
     }
 }
 
 @Composable
-fun MarsTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+fun MarsTopAppBar(
+    currentScreen: MarsPhotosScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    onShareButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     CenterAlignedTopAppBar(
-        scrollBehavior = scrollBehavior,
         title = {
             Text(
                 text = stringResource(R.string.app_name),
                 style = MaterialTheme.typography.headlineSmall,
             )
         },
-        modifier = modifier
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        },
+        actions = {
+            if (currentScreen == MarsPhotosScreen.Photo) {
+                IconButton(onClick = onShareButtonClicked) {
+                    Icon(imageVector = Icons.Filled.Share, stringResource(R.string.share))
+                }
+            }
+        }
     )
+}
+
+private suspend fun sharePhoto(
+    intentContext: Context,
+    photo: MarsPhoto
+) {
+    val imageFile = downloadImageFromUrl(
+        context = intentContext,
+        url = photo.imgSrc
+    )
+    val imageUri = FileProvider.getUriForFile(
+        intentContext,
+        "${intentContext.packageName}.fileprovider",
+        imageFile
+    )
+
+    val shareIntent = Intent().apply {
+        action = Intent.ACTION_SEND
+        type = "image/jpg"
+        putExtra(
+            Intent.EXTRA_STREAM,
+            imageUri
+        )
+        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    }
+
+    intentContext.startActivity(Intent.createChooser(shareIntent, "Share mars photo!"))
+}
+
+private suspend fun downloadImageFromUrl(
+    context: Context,
+    url: String
+): File {
+    val loader = ImageLoader(context)
+    val request = ImageRequest.Builder(context)
+        .data(url)
+        .build()
+
+    val drawable = withContext(Dispatchers.IO) {
+        loader.execute(request).drawable
+    }
+    val bitmap = (drawable as BitmapDrawable).bitmap
+
+    val imageFile = File(context.cacheDir, "photo.jpg")
+    val outputStream = withContext(Dispatchers.IO) {
+        FileOutputStream(imageFile)
+    }
+    // Hardcoded number '73' because using '100' it makes image file even bigger than original file(?)
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 73, outputStream)
+
+    withContext(Dispatchers.IO) {
+        outputStream.flush()
+    }
+
+    return imageFile
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MarsPhotosAppPreview() {
+    MaterialTheme {
+        MarsPhotosApp()
+    }
 }
